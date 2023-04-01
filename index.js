b0a65f998707bb33b4ed70dbaf6acdaa27c0b06b
mattfritsch <mattfritsch98380@gmail.com>
2023-04-01 14:53:10 +0200

    sort finished and search with fuzzysearch finished

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 
 const { Client } = require('@elastic/elasticsearch')
-const {query, response} = require("express");
 const client = new Client({ node: 'http://localhost:9200' })
 
 app.get(
-    '/games',
+    '/games/from/:from/size/:size',
     async (req, res) => {
+        const from = parseInt(req.params.from);
+        const size = parseInt(req.params.size);
         try{
             const response = await client.search({
                 index: 'steam_games',
-                size: 50,
+                from: from,
+                size: size,
                 body: {
                     query: {
                         "match_all": {}
@@ -30,7 +32,7 @@ app.get(
     '/games/get/:id',
     async (req, res) => {
         try{
-            const { id } = req.params;
+            const id = req.params.id;
             const response = await client.get({
                 index: 'steam_games',
                 id
@@ -45,16 +47,19 @@ app.get(
 )
 
 app.get(
-    '/games/sort/:field/:orderBy',
+    '/games/from/:from/size/:size/sort/:field/order/:order',
     async (req, res) => {
         try{
-            const {field} = '"' + req.params.field + '.keyword' + '"';
-            const {orderBy} = req.params.orderBy;
+            const from = parseInt(req.params.from);
+            const size = parseInt(req.params.size);
+            const field = req.params.field + '.keyword';
+            const order = req.params.order;
             const response = await client.search({
                 index: 'steam_games',
+                from: from,
+                size: size,
                 body: {
-                    size: 50,
-                    sort: [{ field : {"order": orderBy} }],
+                    sort: [{[field]: {order: order}}],
                     query: {
                         "match_all": {},
                     },
@@ -68,5 +73,46 @@ app.get(
     }
 )
 
+app.get(
+    '/games/from/:from/size/:size/search/:search',
+    async (req, res) => {
+        try{
+            const search = req.params.search;
+            const size = parseInt(req.params.size);
+            const response = await client.search({
+                index: 'steam_games',
+                size: size,
+                body: {
+                    query: {
+                        bool: {
+                            should: [
+                                {
+                                    match: {
+                                        name: {
+                                            query: search,
+                                            fuzziness: "AUTO"
+                                        }
+                                    }
+                                },
+                                {
+                                    match_phrase_prefix: {
+                                        name: {
+                                            query: search,
+                                            max_expansions: 50
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            })
+            res.send(response.hits.hits)
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error.message);
+        }
+    }
+)
 app.listen(8080, () => {    console.log("Serveur à l'écoute")})
 
